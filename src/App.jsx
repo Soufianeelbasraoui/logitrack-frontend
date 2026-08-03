6f1b0792736e0d9ab410d0e73761a39fbb497b92
soufiane <soufianeelbasraoui0@gmail.com>
2026-08-03 10:54:01 +0200

    feat: configure application routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Auth/Login/Login";
+import Register from "./pages/Auth/Register/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+      </Routes>
+      <ToastContainer position="top-right" autoClose={2000} theme="colored"/>
+    </BrowserRouter>
   );
 }
 
