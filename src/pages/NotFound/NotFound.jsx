6ae4d19b675b8bb1dfdd38d5d6cf4ec3071a12a2
soufiane <soufianeelbasraoui0@gmail.com>
2026-08-07 12:25:39 +0200

    update

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -10,12 +10,10 @@ function NotFound() {
       <div className="main-content">
         <div className="not-found-container">
           <h3>404</h3>
-
           <h2>Page introuvable</h2>
           <p>
             La page que vous recherchez n'existe pas.
           </p>
-
           <Link to="/dashboard" className="btn btn-primary">
             Retour au dashboard
           </Link>
--- src/pages/clients/Clients.jsx
+++ src/pages/clients/Clients.jsx
@@ -9,16 +9,21 @@ import "./clients.css"
 import "../Style.css"
 import { Link} from "react-router-dom";
 import Loader from "../../components/Loader/Loader";
+import PaginationComponent from "../../components/Pagination/Pagination";
 
 function Clients(){
     const[clients,setClients]=useState([]);
     const [loader, setLoader] = useState(true);
+
+    const [page,setPage]=useState(1);
+    const [totalPages,setTotalPages]=useState(0);
     const user = JSON.parse(localStorage.getItem("user"));
     
    useEffect(() => {
-     api.get("/api/clients?page=0&size=10")
+     api.get(`/api/clients?page=${page-1}&size=10`)
     .then((res) => {
       setClients(res.data.content);
+      setTotalPages(res.data.totalPages)
     })
     .catch((err) => {
       console.log(err);
@@ -26,7 +31,7 @@ function Clients(){
     .finally(() => {
       setLoader(false);
     });
-   }, []);
+   }, [page]);
   const handelDelet = async (id) => {
   try {
     await api.delete(`/api/clients/${id}`);
@@ -35,6 +40,11 @@ function Clients(){
     console.log(err);
   }
 };
+
+const handlePageChange = (newPage) => {
+  setPage(newPage);
+};
+
 if(loader){
   return <Loader/>
 }
@@ -91,6 +101,11 @@ if(loader){
                   ))}
                 </tbody>
               </table>
+              <PaginationComponent
+                   page={page}
+                   totalPages={totalPages}
+                   onPageChange={handlePageChange}
+              />
             </div>
         </main>
 
